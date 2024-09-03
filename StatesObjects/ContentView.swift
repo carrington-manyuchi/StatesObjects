90bd9cc5dd0b82baae1a0f98da33bb292fc16e6d
Carrington Manyuchi <carrington.manyuchi@standardbank.co.za>
2024-09-03 21:06:20 +0200

    List View

--- StatesObjects/ContentView.swift
+++ StatesObjects/ContentView.swift
@@ -7,18 +7,59 @@
 
 import SwiftUI
 
+
+
 struct ContentView: View {
+    
+    @State var fruitArray: [FruitModel] = []
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        NavigationStack {
+            List {
+                ForEach(fruitArray) { fruit in
+                    HStack {
+                        Text("\(fruit.count)")
+                            .foregroundStyle(.red)
+                        Text(fruit.name)
+                            .font(.headline)
+                            .bold()
+                    }
+                }
+                .onDelete(perform: deleteFruit(index:))
+            }
+            .listStyle(.insetGrouped)
+            .navigationTitle("Fruit List")
+            .onAppear {
+                getFruits()
+            }
         }
-        .padding()
+    }
+    
+    func getFruits() {
+        let fruit1 = FruitModel(name: "Orange", count: 1)
+        let fruit2 = FruitModel(name: "Banana", count: 2)
+        let fruit3 = FruitModel(name: "Watermelon", count: 88)
+        let fruit12 =   FruitModel(name: "Apples", count: 5)
+        
+        fruitArray.append(fruit1)
+        fruitArray.append(fruit2)
+        fruitArray.append(fruit3)
+        fruitArray.append(fruit12)
+    }
+    
+    func deleteFruit(index: IndexSet) {
+        fruitArray.remove(atOffsets: index)
     }
 }
 
 #Preview {
     ContentView()
+}
+
+
+
+struct FruitModel: Identifiable {
+    let id: String = UUID().uuidString
+    let name: String
+    let count: Int
 }
