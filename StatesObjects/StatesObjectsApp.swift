53fb83e825115458e60336c8d8bba5912bc31d66
Carrington Manyuchi <carrington.manyuchi@standardbank.co.za>
2024-09-03 18:13:02 +0200

    Initial Commit

---
+++ StatesObjects/StatesObjectsApp.swift
@@ -0,0 +1,17 @@
+//
+//  StatesObjectsApp.swift
+//  StatesObjects
+//
+//  Created by Manyuchi, Carrington C on 2024/09/03.
+//
+
+import SwiftUI
+
+@main
+struct StatesObjectsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
